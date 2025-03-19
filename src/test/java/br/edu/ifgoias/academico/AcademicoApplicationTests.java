a9e31e626caaac7f703d9e0b0453800ed85f182f
Felipinho190 <163358644+Felipinho190@users.noreply.github.com>
2025-03-19 01:33:51 -0300

    Update AcademicoApplicationTests.java

--- src/test/java/br/edu/ifgoias/academico/AcademicoApplicationTests.java
+++ src/test/java/br/edu/ifgoias/academico/AcademicoApplicationTests.java
@@ -8,6 +8,8 @@ class AcademicoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	// Este método é usado para verificar se o contexto da aplicação Spring é carregado corretamente.
+        // Não há necessidade de adicionar lógica adicional neste método.
 	}
 
 }
