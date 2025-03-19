aab510a73efaef8e134a9b552ff264fc116aa22b
Felipinho190 <163358644+Felipinho190@users.noreply.github.com>
2025-03-19 01:21:51 -0300

    Update AlunoService.java

--- src/main/java/br/edu/ifgoias/academico/services/AlunoService.java
+++ src/main/java/br/edu/ifgoias/academico/services/AlunoService.java
@@ -38,9 +38,9 @@ public class AlunoService {
 
 	public Aluno update(Integer id, Aluno objAlterado) {
 		return alunoRep.findById(id).map(alunoDB -> {
-			alunoDB.setNome(obj_alterado.getNome());
-			alunoDB.setSexo(obj_alterado.getSexo());
-			alunoDB.setDt_nasc(obj_alterado.getDt_nasc());
+			alunoDB.setNome(objAlterado.getNome());
+			alunoDB.setSexo(objAlterado.getSexo());
+			alunoDB.setDtNasc(objAlterado.getDtNasc());
 			return alunoRep.save(alunoDB);
 		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
