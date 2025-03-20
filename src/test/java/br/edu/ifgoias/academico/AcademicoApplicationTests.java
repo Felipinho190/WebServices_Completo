6341d8f189e64406d2259d09cccb4f4a66c7b470
Felipinho190 <163358644+Felipinho190@users.noreply.github.com>
2025-03-20 20:16:38 -0300

    Update AcademicoApplicationTests.java

--- src/test/java/br/edu/ifgoias/academico/AcademicoApplicationTests.java
+++ src/test/java/br/edu/ifgoias/academico/AcademicoApplicationTests.java
@@ -4,12 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class AcademicoApplicationTests {
+class AcademicoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	// Este método é usado para verificar se o contexto da aplicação Spring é carregado corretamente.
-        // Não há necessidade de adicionar lógica adicional neste método.
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
