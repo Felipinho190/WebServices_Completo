358d65afd3c9626656d638d5d5bad7cb31eb90ae
Felipinho190 <163358644+Felipinho190@users.noreply.github.com>
2025-03-20 20:53:41 -0300

    Update AcademicoApplicationTests.java

--- src/test/java/br/edu/ifgoias/academico/AcademicoApplicationTests.java
+++ src/test/java/br/edu/ifgoias/academico/AcademicoApplicationTests.java
@@ -3,7 +3,8 @@ package br.edu.ifgoias.academico;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.location=classpath:/application-test.properties")
+
 class AcademicoApplicationTests {
 
     @Test
