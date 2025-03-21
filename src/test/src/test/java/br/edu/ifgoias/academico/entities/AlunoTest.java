848187243ac981a648aa013156b35382b78f9298
Felipinho190 <163358644+Felipinho190@users.noreply.github.com>
2025-03-21 10:17:29 -0300

    Update AlunoTest.java

--- src/test/src/test/java/br/edu/ifgoias/academico/entities/AlunoTest.java
+++ src/test/src/test/java/br/edu/ifgoias/academico/entities/AlunoTest.java
@@ -1,21 +1,32 @@
-package br.edu.ifgoias.academico.entities; // Pacote correspondente à classe Aluno
+package br.edu.ifgoias.academico.entities;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Date;
+import java.util.Calendar;
 
 class AlunoTest {
 
     @Test
     void testGetStatusAdulto() {
+        // Cria um aluno com idade >= 18
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.YEAR, -20); // Idade 20 anos
         Aluno aluno = new Aluno();
-        aluno.setIdade(20); // Idade >= 18
-        assertEquals("Adulto", aluno.getStatus()); // Testa o branch "if"
+        aluno.setDtNasc(cal.getTime());
+
+        assertEquals("Adulto", aluno.getStatus());
     }
 
     @Test
     void testGetStatusMenorDeIdade() {
+        // Cria um aluno com idade < 18
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.YEAR, -16); // Idade 16 anos
         Aluno aluno = new Aluno();
-        aluno.setIdade(16); // Idade < 18
-        assertEquals("Menor de idade", aluno.getStatus()); // Testa o branch "else"
+        aluno.setDtNasc(cal.getTime());
+
+        assertEquals("Menor de idade", aluno.getStatus());
     }
 }
