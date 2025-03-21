abbcd716ecb6b3338f0aae075666fc8faf949f7c
Felipinho190 <163358644+Felipinho190@users.noreply.github.com>
2025-03-21 10:48:13 -0300

    Update Curso.java

--- src/main/java/br/edu/ifgoias/academico/entities/Curso.java
+++ src/main/java/br/edu/ifgoias/academico/entities/Curso.java
@@ -15,65 +15,95 @@ import jakarta.persistence.OneToMany;
 
 @Entity
 public class Curso implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer idcurso;
-	
-	@Column (name = "nomecurso", nullable= false )
-	private String nomecurso;
-	
-	@OneToMany (mappedBy = "curso")
-	@JsonIgnore
-	private List<Aluno> alunos = new ArrayList<>();
+    private static final long serialVersionUID = 1L;
 
-	public Curso() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idcurso;
 
-	}
-	
-	public Curso(Integer idcurso, String nome) {
-		this.idcurso = idcurso;
-		this.nomecurso = nome;
-	}
+    @Column(name = "nomecurso", nullable = false)
+    private String nomecurso;
 
-	public Integer getIdcurso() {
-		return idcurso;
-	}
+    @OneToMany(mappedBy = "curso")
+    @JsonIgnore
+    private List<Aluno> alunos = new ArrayList<>();
 
-	public void setIdcurso(Integer idcurso) {
-		this.idcurso = idcurso;
-	}
+    // Construtor padrão
+    public Curso() {}
 
-	public String getNomecurso() {
-		return nomecurso;
-	}
+    // Construtor com parâmetros
+    public Curso(Integer idcurso, String nomecurso) {
+        this.idcurso = idcurso;
+        this.nomecurso = nomecurso;
+    }
 
-	public void setNomecurso(String nome) {
-		this.nomecurso = nome;
-	}
+    // Getters e Setters
+    public Integer getIdcurso() {
+        return idcurso;
+    }
 
-	public List<Aluno> getAlunos() {
-		return alunos;
-	}
+    public void setIdcurso(Integer idcurso) {
+        this.idcurso = idcurso;
+    }
 
-	public void addAluno (Aluno a) {
-			this.alunos.add(a);
-			a.setCurso(this);
-	}
-	
-	public void removeAluno (Aluno a) {
-		
-		if (this.alunos.contains(a)) {
-			this.removeAluno(a);
-			a.setCurso(null);
-		}
-	}
+    public String getNomecurso() {
+        return nomecurso;
+    }
 
-	@Override
-	public String toString() {
-		return "Curso [idcurso=" + idcurso + ", nomecurso=" + nomecurso + "]";
-	}
-		
+    public void setNomecurso(String nomecurso) {
+        this.nomecurso = nomecurso;
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    // Método para adicionar um aluno ao curso
+    public void addAluno(Aluno aluno) {
+        if (!this.alunos.contains(aluno)) {
+            this.alunos.add(aluno);
+            aluno.setCurso(this); // Garante a consistência da relação bidirecional
+        }
+    }
+
+    // Método para remover um aluno do curso
+    public void removeAluno(Aluno aluno) {
+        if (this.alunos.contains(aluno)) {
+            this.alunos.remove(aluno); // Corrige o loop infinito
+            aluno.setCurso(null); // Remove a referência ao curso
+        }
+    }
+
+    // Representação textual do objeto
+    @Override
+    public String toString() {
+        return "Curso [idcurso=" + idcurso + ", nomecurso=" + nomecurso + "]";
+    }
+
+    // Métodos hashCode e equals (opcional, mas recomendado para comparação de objetos)
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((idcurso == null) ? 0 : idcurso.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Curso other = (Curso) obj;
+        if (idcurso == null) {
+            if (other.idcurso != null)
+                return false;
+        } else if (!idcurso.equals(other.idcurso))
+            return false;
+        return true;
+    }
 }
